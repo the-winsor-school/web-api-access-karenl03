c1e128c200b8e7966889cf9e8eda77b586a52ac0
Karen Li <36568510+karenl03@users.noreply.github.com>
2020-02-26 12:02:52 -0500

    recipes

--- InternetData/Program.cs
+++ InternetData/Program.cs
@@ -2,67 +2,29 @@
 using System.Collections.Generic;
 using System.Runtime.Serialization.Json;
 using System.Net.Http;
+using InternetData.Properties;
 
 namespace InternetData
 {
     class MainClass
     {
-        public static void GetRonSwansonQuotes()
+        public static void GetRecipesFromSearch()
         {
             HttpClient client = new HttpClient();
 
-            Console.Write("How many quotes do you want?  ");
-            int count = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("What meal do you want to search for?  ");
+            //int count = Convert.ToInt32(Console.ReadLine());
+            string meal = "pasta";
 
-            // Create an HTTP GET request for the API Endpoint.
-            HttpRequestMessage request =
-                new HttpRequestMessage(HttpMethod.Get,
-                // This is the URL for the API endpoint, the last {0} is where you insert the number /count/ into the string.
-                string.Format("https://ron-swanson-quotes.herokuapp.com/v2/quotes/{0}", count));
-
-            // Use the HttpClient to send the request message to the remote server.
-            // The result, is a Response message which contains the data you requested.
-            HttpResponseMessage response = client.SendAsync(request).Result;
-
-            // Since the data is in JSON format, we use a DataContractJsonSerializer to pull the data out of that object.
-            // the JsonSerializer takes a DataType as its input.  in this case, we expect the response to be a List of Strings.
-            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(List<string>));
-
-            // Make sure the response is actually there and not a failed request.
-            if (response.IsSuccessStatusCode)
-            {
-                // Read the data from the response message.  The result is an Object that is converted into a List<string> type.
-                List<string> quotes = (List<string>)serializer.ReadObject(response.Content.ReadAsStreamAsync().Result);
-
-                // Print each of the quotes!
-                foreach (string quote in quotes)
-                {
-                    Console.WriteLine(quote);
-                }
-            }
-
-            // If something went wrong...
-            else
-            {
-                // Make the text red
-                Console.ForegroundColor = ConsoleColor.Red;
-
-                // print the status code for the failure.  see https://http.cat for interpretations
-                Console.WriteLine("Failed!  Status Code: {0}", response.StatusCode);
-            }
+            RecipeInfo recipe = Recipes.getRecipeInfo(meal);
+            
+            Console.WriteLine(recipe.strMeal);
         }
 
 
         public static void Main(string[] args)
         {
-            //WeatherForcast forcast = Weather.GetWeatherForcast();
-
-            //Console.WriteLine(forcast.currently.summary);
-
-            //Console.WriteLine("\nDone!");
-
-            GetRonSwansonQuotes();
-            Console.WriteLine("\nDone!");
+            GetRecipesFromSearch();
         }
     }
 }
---
+++ InternetData/Properties/Recipes.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace InternetData.Properties
+{
+    public class Recipes
+    {
+        public static RecipeInfo getRecipeInfo(string meal)
+        {
+            HttpClient client = new HttpClient();
+
+            HttpRequestMessage request = new HttpRequestMessage(
+                HttpMethod.Get,
+                string.Format("https://www.themealdb.com/api/json/v1/1/search.php?s={0}", meal));
+
+            HttpResponseMessage response = client.SendAsync(request).Result;
+
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(RecipesList));
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return new RecipeInfo();
+            }
+
+            return ((RecipesList)serializer.ReadObject(response.Content.ReadAsStreamAsync().Result)).meals[0];
+        }
+    }
+
+    [DataContract]
+    public class RecipesList
+    {
+        [DataMember]
+        public List<RecipeInfo> meals;
+    }
+
+    [DataContract]
+    public class RecipeInfo //declaring the class
+    {
+        [DataMember] //decoration (everything that comes from the API)
+        public string strMeal; //property name must be identical to name in JSON file
+
+        [DataMember]
+        public string strCategory;
+
+        [DataMember]
+        public string strInstructions;
+
+        [DataMember]
+        public string strSource;
+
+        //string ingredients
+        [DataMember]
+        public string strIngredient1;
+
+        [DataMember]
+        public string strIngredient2;
+
+        [DataMember]
+        public string strIngredient3;
+
+        [DataMember]
+        public string strIngredient4;
+
+        [DataMember]
+        public string strIngredient5;
+
+        [DataMember]
+        public string strIngredient6;
+
+        [DataMember]
+        public string strIngredient8;
+
+        [DataMember]
+        public string strIngredient9;
+
+        [DataMember]
+        public string strIngredient10;
+
+        [DataMember]
+        public string strIngredient11;
+
+        [DataMember]
+        public string strIngredient12;
+
+        [DataMember]
+        public string strIngredient13;
+
+        [DataMember]
+        public string strIngredient14;
+
+        [DataMember]
+        public string strIngredient15;
+
+        [DataMember]
+        public string strIngredient16;
+
+        [DataMember]
+        public string strIngredient17;
+
+        [DataMember]
+        public string strIngredient18;
+
+        [DataMember]
+        public string strIngredient19;
+
+        [DataMember]
+        public string strIngredient20;
+
+        //string measures
+        [DataMember]
+        public string strMeasure1;
+
+        [DataMember]
+        public string strMeasure2;
+
+        [DataMember]
+        public string strMeasure3;
+
+        [DataMember]
+        public string strMeasure4;
+
+        [DataMember]
+        public string strMeasure5;
+
+        [DataMember]
+        public string strMeasure6;
+
+        [DataMember]
+        public string strMeasure7;
+
+        [DataMember]
+        public string strMeasure8;
+
+        [DataMember]
+        public string strMeasure9;
+
+        [DataMember]
+        public string strMeasure10;
+
+        [DataMember]
+        public string strMeasure11;
+
+        [DataMember]
+        public string strMeasure12;
+
+        [DataMember]
+        public string strMeasure13;
+
+        [DataMember]
+        public string strMeasure14;
+
+        [DataMember]
+        public string strMeasure15;
+
+        [DataMember]
+        public string strMeasure16;
+
+        [DataMember]
+        public string strMeasure17;
+
+        [DataMember]
+        public string strMeasure18;
+
+        [DataMember]
+        public string strMeasure19;
+
+        [DataMember]
+        public string strMeasure20;
+    }
+}
